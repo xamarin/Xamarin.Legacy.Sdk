6bcabe9d6ff54605332bd8ef66a9de947576e0e2
Filip Navara <navara@emclient.com>
2023-01-18 00:29:04 +0100

    Add sample that references Xamarin.Forms

---
+++ samples/HelloForms/Main.cs
@@ -0,0 +1,15 @@
+using Xamarin.Forms;
+
+// Use an attribute from the Xamarin.Forms library
+[assembly: ResolutionGroupName("Anything")]
+
+namespace Hello
+{
+    public class Application
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+}
